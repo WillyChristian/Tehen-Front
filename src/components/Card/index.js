424a88fb92e64627b5497739fbfd5fbc2b65ac37
Soleph <nathan.nahand@gmail.com>
2020-02-26 17:08:58 -0300

    Added pagination posts

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,17 +6,41 @@ import api from '../../services/api';
 export default class Card extends Component {
   state = {
     posts: [],
+    postInfo: {},
+    page: 1,
+    totalPages: {},
   };
 
   componentDidMount() {
     this.loadPosts();
   }
 
-  loadPosts = () => {
-    api.get('/posts').then((res) => {
-      const posts = res.data;
-      this.setState({ posts });
-    });
+  loadPosts = async (page = 1) => {
+    const response = await api.get(`/posts?page=${page}`);
+
+    const { docs, postInfo, totalPages } = response.data;
+
+    this.setState({ posts: docs, postInfo, page, totalPages });
+  };
+
+  prevPage = () => {
+    const { page } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadPosts(pageNumber);
+  };
+
+  nextPage = () => {
+    const { totalPages, page } = this.state;
+
+    if (page === totalPages) return;
+
+    const pageNumber = page + 1;
+
+    this.loadPosts(pageNumber);
   };
 
   render() {
@@ -31,6 +55,14 @@ export default class Card extends Component {
             </PostCard>
           ))}
         </Section>
+        <div>
+          <button type="button" onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button type="button" onClick={this.nextPage}>
+            Proximo
+          </button>
+        </div>
       </Container>
     );
   }
