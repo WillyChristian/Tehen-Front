d9e250a8f05a909b5b83f4f3c9d1acc76d61b3c6
Soleph <nathan.nahand@gmail.com>
2020-03-01 18:32:35 -0300

    Remove Login page

--- src/Routes.js
+++ src/Routes.js
@@ -3,13 +3,11 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import Post from './pages/Post';
-import Login from './pages/Login';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Main} />
-      <Route path="/admin/login" exact component={Login} />
       <Route path="/posts/:id" component={Post} />
     </Switch>
   </BrowserRouter>
